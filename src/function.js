f3ea55f72d0ad6512a519ef87770873c76ecfa9b
alanarv99 <147183094+alanarv99@users.noreply.github.com>
2023-11-08 20:00:10 -0500

    added  current date and time elements to javascript

--- src/function.js
+++ src/function.js
@@ -6,8 +6,12 @@ function displayWeather(response) {
   let weatherWind = document.querySelector("#wind");
   let weatherDescriptor = document.querySelector("#description");
   let weatherIcon = document.querySelector("#icon");
+  let celciusTemperature = response.data.main.temp;
+  let timeElement = document.querySelector("#dateTime");
+  let date = new Date();
 
-  temp.innerHTML = Math.round(response.data.main.temp);
+  timeElement.innerHTML = formatDate(date);
+  temp.innerHTML = Math.round(celciusTemperature);
   cityElement.innerHTML = response.data.name;
   weatherCloud.innerHTML = ` ${Math.round(response.data.clouds.all)}`;
   weatherHumidity.innerHTML = ` ${Math.round(response.data.main.humidity)}%`;
@@ -30,18 +34,59 @@ function latLonLocation(event) {
   }
   navigator.geolocation.getCurrentPosition(pinLocation);
 }
-let pin = document.querySelector("#currentLocation");
-pin.addEventListener("click", latLonLocation);
-
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let dater = date.getDate();
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${month} ${dater} at ${hour}:${minutes}`;
+}
 function search(city) {
   let apiKey = "5263648a192779a590c660d9fb890f22";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeather);
 }
+
 function searchedCity(event) {
   event.preventDefault();
   let cityEntered = document.querySelector("#searchBar");
   search(cityEntered.value);
 }
+
+let pin = document.querySelector("#currentLocation");
+pin.addEventListener("click", latLonLocation);
+
 let searcher = document.querySelector("#searchCityForm");
 searcher.addEventListener("submit", searchedCity);
+
+search("New York");
